2b803965e8851ca1dc2de5ee0ab4d13e4ee03b2e
basic <basic@E7>
2016-03-28 20:53:31 +0900

    for/break/continue/array

--- java.Project/src/Sample_0321/Calculator.java
+++ java.Project/src/Sample_0321/Calculator.java
@@ -9,32 +9,30 @@ public class Calculator {
 		int x = 10, y = 20, result;
 		result = x + y;
 		System.out.println(result);
-		
-		Scanner input = new Scanner(System.in); //¹®(±Í)À» ¿­·¶¾î!!
-		
+
+		Scanner input = new Scanner(System.in); // ¹®(±Í)À» ¿­·¶¾î!!
+
 		int a, b, c;
-		a = input.nextInt(); 
+		a = input.nextInt();
 		b = input.nextInt();
-		
+
 		c = a * b;
-		
-		System.out.println("multi : "+ c);
-		
-		input.close();  //¹®(±Í)À» ´Ý¾Æ¾ßÇØ!! (ÇÊ¼ö´Â ¾Æ´Ï¾ß)
-		
+
+		System.out.println("multi : " + c);
+
+		input.close(); // ¹®(±Í)À» ´Ý¾Æ¾ßÇØ!! (ÇÊ¼ö´Â ¾Æ´Ï¾ß)
 
 	}
 
 }
 
-//		int i, j;
-//		i = 1;
-//		j = ++i; >> i=1+1=2 & j=2
-		
-//		int i, j;
-//		i = 1;
-//		j = i++; >> j=1 & i=1+1=2
+// int i, j;
+// i = 1;
+// j = ++i; >> i=1+1=2 & j=2
 
-//		x=1
-//      y=++x + x++ + --x + x++ + ++x >> x=1+1-1+1=2 & y=10
-		
+// int i, j;
+// i = 1;
+// j = i++; >> j=1 & i=1+1=2
+
+// x=1
+// y=++x + x++ + --x + x++ + ++x >> x=1+1-1+1=2 & y=10
--- java.Project/src/Sample_0321/For_statement.java
+++ java.Project/src/Sample_0321/For_statement.java
@@ -5,10 +5,10 @@ public class For_statement {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for(int i=0 ; i<10 ; i++){      //for(초기화;조건식;증감식)
-			System.out.println("value :"+i);
+		for (int i = 0; i < 10; i++) { // for(초기화;조건식;증감식)
+			System.out.println("value :" + i);
 		}
 	}
 
 }
-//다음시간에 이중for문
+// 다음시간에 이중for문
