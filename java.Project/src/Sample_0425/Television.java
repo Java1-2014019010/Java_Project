784e421409a42f40af181ecb8bbe97623b607b25
basic <basic@E7>
2016-04-25 20:46:54 +0900

    TelevisionTest

---
+++ java.Project/src/Sample_0425/Television.java
@@ -0,0 +1,18 @@
+package Sample_0425;
+
+public class Television {
+	private int channel; // 채널 번호
+	private int volume; // 볼륨
+	private boolean onOff; // 전원 상태
+
+	Television(int c, int v, boolean o) {
+		channel = c;
+		volume = v;
+		onOff = o;
+	}
+
+	void print() {
+		System.out.println("채널은 " + channel + "이고 볼륨은 " + volume + "입니다.");
+	}
+
+}
---
+++ java.Project/src/Sample_0425/TelevisionTest.java
@@ -0,0 +1,13 @@
+package Sample_0425;
+
+public class TelevisionTest {
+
+	public static void main(String[] args) {
+		
+		Television myTv = new Television(7, 10, true); 
+		myTv.print();
+		Television yourTv = new Television(7, 10, true); 
+		yourTv.print();
+	}
+
+}
