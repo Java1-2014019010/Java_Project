b3ca63c3876e5789bfc0cf4ea93467b5b595097d
basic <basic@E7>
2016-05-09 19:22:00 +0900

    static member

--- java.Project/src/Sample_0502/CarTest.java
+++ java.Project/src/Sample_0502/CarTest.java
@@ -2,13 +2,13 @@ package Sample_0502;
 
 public class CarTest {
 
-	//Car c1 = new Car("1","2",3); ¾È‰Î!!
+	static Car c1 = new Car("1","2",3);// ¾È‰Î!!
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(Car.numbers);
 		
-		Car c1 = new Car("s600","white",80);
+		//Car c1 = new Car("s600","white",80);
 		
 		System.out.println(Car.numbers);
 		
