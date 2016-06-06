c62b95afb11a19dd73dfd7ac37c25eed3dcb2c30
basic <basic@E7>
2016-06-06 17:18:53 +0900

    checkbox

---
+++ java.Project/src/Sample_0606/CheckBox.java
@@ -0,0 +1,35 @@
+package Sample_0606;
+
+import java.awt.GridLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class CheckBox extends JPanel implements ItemListener {
+	JCheckBox[] buttons = new JCheckBox[3];
+	String[] fruits = {"apple", "grape", "orange"};
+	JLabel[] pictureLabel = new JLabel[3];
+	
+	public CheckBox(){
+		super(new GridLayout(0,4));
+		
+		for (){
+			
+		}
+		
+		JPanel checkPanel = new
+	}
+
+	pulic void itemStateChanged(ItemEvent){
+		
+	}
+	
+	public static void main(String[] args) {
+		JFrame
+	}
+
+
+}
